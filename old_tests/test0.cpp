033457768c879449f26ae97360967c98d042beb3
jsyschan <jchan5@DESKTOP-N4FM4NO.localdomain>
2019-06-26 11:55:25 -0400

    Finished Basic NN with examp.

---
+++ old_tests/test0.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+
+	int *X; // = 7;
+	//cout << "X is " << X << endl << endl;
+	&X = 99;
+	cout << "X is now " << X << endl << endl;
+	return 0;
+}
